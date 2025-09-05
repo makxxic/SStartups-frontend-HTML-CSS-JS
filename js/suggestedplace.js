95d3aa97abcac54b0724a725cedbcd0cb1b6b6f9
McMnyages <morachasilas@gmail.com>
2025-09-05 19:51:03 +0300

    Added the suggestion html,css,js and navbar and footer and dummy data

---
+++ js/suggestedplace.js
@@ -0,0 +1,21 @@
+function openModal(src, alt) {
+    const modal = document.getElementById('modal');
+    const modalImg = document.getElementById('modal-img');
+    const captionText = document.getElementById('caption');
+    modal.style.display = 'flex';
+    modalImg.src = src;
+    captionText.innerHTML = alt;
+}
+
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+
+// Close modal when clicking outside the image
+document.addEventListener('click', (e) => {
+    const modal = document.getElementById('modal');
+    if (e.target === modal) {
+        closeModal();
+    }
+});
